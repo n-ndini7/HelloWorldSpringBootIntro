75ef6d7f3ba219dda580c7693eb90117c6237799
n-ndini7 <hp@RJs-PC>
2020-11-26 14:06:39 +0530

    [Nandini] Added master

---
+++ src/main/java/com/example/demo/DTO/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.DTO;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	
+	public String getlastName() {
+		return lastName;
+	}
+	public void setlastName(String lName) {
+		this.lastName = lName;
+	}
+	public String getfirstName() {
+		return firstName;
+	}
+	public void setfirstName(String fName) {
+		this.firstName = fName;
+	}
+
+}
